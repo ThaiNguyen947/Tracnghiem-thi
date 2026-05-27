3544fa8a93e78a881706eb14034e658079e025f3
ThaiNguyen947 <mtnaxie@gmail.com>
2026-05-27 11:37:07 +0700

    Update app.js

--- app.js
+++ app.js
@@ -161,6 +161,7 @@ async function taiTatCaDuLieuCauHoi() {
     "./cau_hoi/p7.json",
     "./cau_hoi/p8.json",
      "./cau_hoi/p9.json",
+    "./cau_hoi/p9.json",
     "./cau_hoi/pcuathay.json"
   ];
   
